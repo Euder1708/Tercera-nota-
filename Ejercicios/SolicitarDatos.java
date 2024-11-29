de6a056179da5ce2fca6240717844b075c898eeb
Euder1708 <ejpachecoa@ufpso.edu.co>
2024-11-29 10:51:40 -0500

    first commit

---
+++ Ejercicios/SolicitarDatos.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+//Programa que solicite datos de usuario 
+public class SolicitarDatos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        
+        scanner.close();
+    }
+}
